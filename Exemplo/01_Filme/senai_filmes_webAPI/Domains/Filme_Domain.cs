2f61bc569d24f777ba2db8e1da00a8864dce3974
Leonardo Souza <leonardo030605@gmail.com>
2021-08-24 12:41:46 -0300

    Exemplo 1 Back-End

---
+++ Exemplo/01_Filme/senai_filmes_webAPI/Domains/Filme_Domain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_filmes_webAPI.Domains
+{
+    /// <summary>
+    /// Essa classe representa a entidade Filme
+    /// </summary>
+    public class Filme_Domain
+    {
+        public int Id_Filme { get; set; }
+
+        public int Id_Genero { get; set; }
+
+        public string Nome_Filme { get; set; }
+    }
+}
---
+++ Exemplo/01_Filme/senai_filmes_webAPI/Domains/Genero_Domain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_filmes_webAPI.Domains
+{
+    /// <summary>
+    /// Classe que representa a entidade Genero
+    /// </summary>
+    public class Genero_Domain
+    {
+        public int Id_Genero { get; set; }
+
+        public string Nome_Genero { get; set; }
+    }
+}
