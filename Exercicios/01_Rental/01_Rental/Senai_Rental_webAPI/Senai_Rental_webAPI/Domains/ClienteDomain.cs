83f9a43699332d41e30da942b8e1f3edd8adfc84
Leonardo Souza <leonardo030605@gmail.com>
2021-08-31 21:53:43 -0300

    Criação dos Domains

---
+++ Exercicios/01_Rental/01_Rental/Senai_Rental_webAPI/Senai_Rental_webAPI/Domains/ClienteDomain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_Rental_webAPI.Domains
+{
+    public class ClienteDomain
+    {
+        public int IdCliente { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public int CPF { get; set; }
+    }
+}
---
+++ Exercicios/01_Rental/01_Rental/Senai_Rental_webAPI/Senai_Rental_webAPI/Domains/EmpresaDomain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_Rental_webAPI.Domains
+{
+    public class EmpresaDomain
+    {
+        public int IdEmpresa { get; set; }
+
+        public string Nome_Empresa { get; set; }
+    }
+}
